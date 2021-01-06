681cb1b5c7a9cc0b03f14406c254b2f68a5cd5e8
swastia <44293051+swastia@users.noreply.github.com>
2021-01-06 18:56:30 +0530

    Netflix Eureka Service Discovery App

---
+++ serviceDiscoveryApp/src/main/java/com/sarya/serviceDiscoveryApp/ServiceDiscoveryAppApplication.java
@@ -0,0 +1,15 @@
+package com.sarya.serviceDiscoveryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceDiscoveryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryAppApplication.class, args);
+	}
+
+}
